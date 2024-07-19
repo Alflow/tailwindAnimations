27f0c7d2369a83f722ac4742df0d6155b26ce031
Alfredo Alberruche <aalberrh@gmail.com>
2024-07-19 17:34:20 +0200

    first config.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-console.log("Hello world")
-console.log("Hello world")
+import createPlugin from 'tailwindcss/plugin.js'
+import theme from './theme.js'
+
+/** @type {import ('tailwindcss/types/config').PluginCreator}} */
+const pluginCreator = api => {
+    console.log("Plugin creator")
+}
+
+/** @type {import ('tailwindcss/types/config').Config}  */
+const pluginConfig = {theme}
+
+
+export default createPlugin(pluginCreator, pluginConfig)
